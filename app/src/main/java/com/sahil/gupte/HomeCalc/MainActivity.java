d99ce8bf1867e2d38beeab751bc29f7dad471ed6
Ovenoboyo <ovenoboyo@gmail.com>
2019-10-16 00:26:41 +0530

    Handle permission request results
    Signed-off-by: Ovenoboyo <ovenoboyo@gmail.com>

--- app/src/main/java/com/sahil/gupte/HomeCalc/MainActivity.java
+++ app/src/main/java/com/sahil/gupte/HomeCalc/MainActivity.java
@@ -162,6 +162,21 @@ public class MainActivity extends AppCompatActivity
 
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String[] permissions, int[] grantResults) {
+
+
+        // If request is cancelled, the result arrays are empty.
+        if (grantResults.length > 0
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+            recreate();
+        } else {
+            finishAndRemoveTask();
+        }
+    }
+
     //sign out method
     private void signOut() {
         auth.signOut();
